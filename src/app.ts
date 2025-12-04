e44ca7a0b678bfacaad9d2bc5e98396d44c5c54b
Roman Maslennikov <romzes120282@gmail.com>
2025-12-04 15:46:43 +0300

    corrected package.json

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,6 @@ const { PORT = 3003 } = process.env;
 const app = express();
 
 app.listen(PORT, () => {
-    // Если всё работает, консоль покажет, какой порт приложение слушает
-    console.log(`App listening on port ${PORT}`)
-})
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`);
+});
