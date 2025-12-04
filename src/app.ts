77bb03232131237351b2d49144bb31838cef4299
Roman Maslennikov <romzes120282@gmail.com>
2025-12-04 18:49:44 +0300

    models done

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
 // Слушаем 3003 порт
 const { PORT = 3003 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
